48915ad94894ead7fbaf6ddfa6efd8e86672feb2
Anuranjan Gari <anuranjan.gari08@gmail.com>
2024-03-26 18:04:58 +0100

    process variables

--- app.js
+++ app.js
@@ -26,8 +26,7 @@ const ExpressError = require("./utils/ExpressError");
 
 const app = express();
 
-const dbUrl = process.env.DB_URL;
-//"mongodb://localhost:27017/yelp-camp";
+const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/yelp-camp";
 //
 
 mongoose.connect(dbUrl, {
