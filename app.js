4bc5d4d8d89ffb756567191cd5cd16e65fb402ae
Anuranjan Gari <anuranjan.gari08@gmail.com>
2024-03-26 16:00:10 +0100

    Atlas

--- app.js
+++ app.js
@@ -26,8 +26,9 @@ const ExpressError = require("./utils/ExpressError");
 
 const app = express();
 
-const dbUrl = "mongodb://localhost:27017/yelp-camp";
-//process.env.DB_URL;
+const dbUrl = process.env.DB_URL;
+//"mongodb://localhost:27017/yelp-camp";
+//
 
 mongoose.connect(dbUrl, {
   //useNewUrlParser: true,
